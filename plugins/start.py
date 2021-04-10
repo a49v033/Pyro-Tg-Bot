f187f71ccb9f5235252b0db5ac26935d01950d65
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-04-10 22:50:33 +0530

    lo

---
+++ plugins/start.py
@@ -0,0 +1,27 @@
+# ( c ) Bruh_0x
+
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@Client.on_message(filters.command(["start", "start@Pyro_Tg_Bot"]))
+async def start(_, message: Message):
+    await message.send_document("BQACAgUAAxkBAAPBYHHdnL_9O2i1Stc49TzTMDb3ZeIAApYCAAIIdpFXJ-zFnfAtGloeBA")
+    await message.reply_text(
+        f"""<b>Hi {message.from_user.first_name} 😉️!</b>
+Above File is sended by Pyro Tg Bot!
+
+Made by **@Bruh_0x** for Noob/Beginners Like Him! (Me) """,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🔰️ My Updates Channel 🔰️", url="https://t.me/NexaBotsUpdates"
+                    ),
+                    InlineKeyboardButton(
+                        "⚜️ Support Group ⚜️", url="https://t.me/Nexa_bots"
+                    )
+                ]
+            ]
+        )
+    )
