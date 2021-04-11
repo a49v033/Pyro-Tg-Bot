9c7b863625513a0f6842607d343df1a6cc926b35
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-04-11 11:08:55 +0530

    should i send it?

--- plugins/start.py
+++ plugins/start.py
@@ -50,4 +50,4 @@ Made by **@Bruh_0x** for Noob/Beginners Like Him! (Me) """,
 
 @Client.on_callback_query(filters.regex("stick"))
 async def stick(_, query: CallbackQuery):
-    await Message.reply_sticker("CAACAgUAAxkBAAIEyGByiGplc4_5-OJ7eujZWlkZx7WoAAKRAQAC7thQV25-qe8JhyOhHgQ")
+    await Message.send_sticker("CAACAgUAAxkBAAIEyGByiGplc4_5-OJ7eujZWlkZx7WoAAKRAQAC7thQV25-qe8JhyOhHgQ")
