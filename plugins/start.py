c0e88d2fac940726c79d36051133f1a18884f645
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-04-11 22:42:24 +0530

    str

--- plugins/start.py
+++ plugins/start.py
@@ -24,32 +24,3 @@ Made by **@Bruh_0x** for Noob/Beginners Like Him! (Me) """,
             ]
         )
     )
-
-    
-@Client.on_message(filters.command(["sticker", "sticker@Pyro_Tg_Bot"]))
-async def sticker(_, message: Message):
-    await message.reply_text(
-        f"""<b>Hi {message.from_user.first_name} 😉️!</b>
-Above File is sended by Pyro Tg Bot!
-
-Made by **@Bruh_0x** for Noob/Beginners Like Him! (Me) """,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "About", callback_data="stick"
-                    )
-                ]
-            ]
-        )
-    )
-    
-    
-    
-# callback shit
-
-@Client.on_callback_query(filters.regex("stick"))
-async def stick(_, query: CallbackQuery):
-    chat_id=Message.chat.id,
-    sticker="CAACAgUAAxkBAAIEyGByiGplc4_5-OJ7eujZWlkZx7WoAAKRAQAC7thQV25-qe8JhyOhHgQ"
-    await Client.send_sticker(sticker)
